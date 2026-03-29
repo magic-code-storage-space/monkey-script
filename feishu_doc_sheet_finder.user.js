e1e673cae179508782101bcc14e1cda616dbc36a
dong.luo <dong.luo@happyelements.com>
2026-03-29 22:39:59 +0800

    feishu_doc_sheet_finder 0.3.6

--- feishu_doc_sheet_finder.user.js
+++ feishu_doc_sheet_finder.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         feishu_doc_sheet_finder
 // @namespace    http://tampermonkey.net/
-// @version      0.3.4
+// @version      0.3.6
 // @description  飞书文档Excel页签搜索框
 // @author       dong.luo@happyelements.com
 // @include      /^http[s]*:\/\/.*\.feishu\.cn\/sheets\/.*$/
@@ -24,18 +24,19 @@
     // 开启定时检查
     setInterval(function() {
         // 初始化搜索框
-        initSearchInput();
+        initSheetSearchInput();
+        initExcelSearchInput();
     }, 500);
 
     // 初始化输入框监听事件
-    function initSearchInputListener() {
-        $("#ld-feishu-doc-finder-text").unbind("input propertychange").bind("input propertychange", function (event) {
+    function initSheetSearchInputListener() {
+        $("#ld-feishu-sheet-finder-text").unbind("input propertychange").bind("input propertychange", function (event) {
             var sheetMenuDiv = $(".all-sheet-list");
             if (!sheetMenuDiv || sheetMenuDiv.size() != 1) {
                 return;
             }
 
-            // var widthPx = $("#ld-feishu-doc-finder").css("width");
+            // var widthPx = $("#ld-feishu-sheet-finder").css("width");
             // sheetMenuDiv.css({"width": widthPx});
 
             var searchVal = $(this).val();
@@ -57,8 +58,36 @@
         });
     }
 
+
+    // 初始化输入框监听事件
+    function initExcelSearchInputListener() {
+        $("#ld-feishu-excel-finder-text").unbind("input propertychange").bind("input propertychange", function (event) {
+            var excelMenuDiv = $(".breadcrumb-dropdown-menu-inner");
+            if (!excelMenuDiv || excelMenuDiv.size() != 1) {
+                return;
+            }
+
+            var searchVal = $(this).val();
+            //console.log("search: "+searchVal)
+            if (!searchVal) {
+                //全部展现
+                $('.breadcrumb-node-list-item', excelMenuDiv).show();
+            } else {
+                $('.breadcrumb-node-list-item', excelMenuDiv).each(function (i) {
+                    var searchData = $(".breadcrumb-container-item__value", $(this)).html();
+                    var matched = searchData && searchData.toLowerCase().indexOf(searchVal.toLowerCase()) >= 0;
+                    if (matched) {
+                        $(this).show();
+                    } else {
+                        $(this).hide();
+                    }
+                });
+            }
+        });
+    }
+
     // 初始化搜索框
-    function initSearchInput() {
+    function initSheetSearchInput() {
         // 页签列表菜单div
         var sheetMenuDiv = $(".all-sheets-wrapper .all-sheet-list");
         if (!sheetMenuDiv || sheetMenuDiv.size() != 1) {
@@ -88,10 +117,10 @@
         $(".sortable-sheet-item .sheet-name", sheetMenuDiv).css({"max-width": "100%"});
 
         // 注入HTML代码
-        if ($("#ld-feishu-doc-finder").size() != 0) {
+        if ($("#ld-feishu-sheet-finder").size() != 0) {
             return false;
         }
-        $("div", sheetMenuDiv).eq(0).before(genFinderHtmlCode()); // 注入代码
+        $("div", sheetMenuDiv).eq(0).before(genSheetFinderHtmlCode()); // 注入代码
 
 
         // 模拟点击查找替换按钮（用来支持Ctrl+V）
@@ -106,10 +135,63 @@
         }
 
         // 初始化输入框监听事件
-        initSearchInputListener();
+        initSheetSearchInputListener();
 
         // 扩展页签列表显示
         handlerSheetListExtraShow();
+
+        // 返回
+        return true;
+    }
+
+
+    function initExcelSearchInput() {
+        // 页签列表菜单div
+        var excelMenuDiv = $(".breadcrumb-dropdown-menu");
+
+        if (!excelMenuDiv || excelMenuDiv.size() != 1) {
+
+            // 模拟点击查找替换按钮（用来支持Ctrl+V）
+            let findbarPanel = $(".findbar[data-sheet-element=sheet_findbar]");
+            if(findbarPanel && findbarPanel.size() == 1) {
+                if(!findbarPanel.hasClass("findbar_hidden") && findbarPanel.attr("ld-sheet-finder-flag") == "1"){
+                    findbarPanel.attr("ld-sheet-finder-flag", "0").removeClass("ld-sheet-finder-hidden"); // 设置为显示、清除模拟点击标识
+                    $("#sheet-find").click(); // 先关闭查找替换面板
+                }
+            }
+
+            // 不需要处理
+            return false;
+        }
+
+        // 初始化标识
+        if (excelMenuDiv.attr("ld-add-class-flag") == "1") {
+            return false;
+        }
+        excelMenuDiv.attr("ld-add-class-flag", "1");
+
+        // 修正列表宽度
+        excelMenuDiv.css({"width":"100%", "max-width": "100%"});
+
+        // 注入HTML代码
+        if ($("#ld-feishu-excel-finder").size() != 0) {
+            return false;
+        }
+        $(".breadcrumb-dropdown-menu-inner", excelMenuDiv).eq(0).before(genExcelFinderHtmlCode()); // 注入代码
+
+        // 模拟点击查找替换按钮（用来支持Ctrl+V）
+        let findbarPanel = $(".findbar[data-sheet-element=sheet_findbar]");
+        if(findbarPanel && findbarPanel.size() == 1) {
+            if(findbarPanel.hasClass("findbar_hidden")){
+                $("#sheet-find").click(); // 先打开查找替换面板
+                findbarPanel.attr("ld-sheet-finder-flag", "1").addClass("ld-sheet-finder-hidden"); // 设置为隐藏、标记为模拟点击
+            }else {
+                $("#findbar__find-input").focus(); // 查找替换面板在已经打开的状态先，让输入框获取焦点
+            }
+        }
+
+        // 初始化输入框监听事件
+        initExcelSearchInputListener();
 
         // 返回
         return true;
@@ -121,9 +203,9 @@
             // H5：配置总表【local/design1】
             "shtcnnjxBfNgfXAYAOoN3ETcM8f" : [
                 // config_server.json
-                {"gacha": "摇一摇活动","time_limit_gift": "限时礼包活动","h5_icon_settings": "活动图标显示配置","h5_shortcut_collect": "快捷收藏","product_wechatgame": "商品配置","product_qq_lightgame": "商品配置-QQ小游戏","product_oppo_quickgame": "商品配置-OPPO小游戏","product_baidu_lightgame": "商品配置-百度小游戏","product_allplatform": "全平台商品配置","goods": "商品清单配置","maxLevel": "最大关卡","compen":"公告配置","jigsaw":"拼图活动","coin_match":"金币大作战活动","goldMatch":"夺宝大冒险活动","game":"游戏通用配置","island":"岛屿配置","level_reward":"关卡奖励","wx_group_share":"加步数分享配置","grayscaleTest":"灰度测试","levelScoreCheck":"分数校验","item":"item配置","prop":"道具配置","baidu_act":"百度清凉活动","friend_pvp":"友谊赛","subscribe":"订阅","friendInteractive":"互动分享","starRewardDraw":"星星抽奖","starRewardDrawItem":"星星抽奖奖品","new_rank":"新排行榜","announcement": "自动公告","adProps": "激励广告","h5_ai_package": "h5_ai礼包","aiGiftBag": "AI礼包参数","groupTest": "正交分组","user_back": "玩家召回","drop_adjust": "掉落优化配置","money_box_product": "存钱罐礼包配置","money_box": "存钱罐通用配置","coast_star": "海滨之星","advertising": "新增视频广告","champion_common": "冠军赛通用配置","champion_ai": "冠军赛ai配置","robot": "机器人配置","level_extend": "关卡配置扩展","ad_task": "广告任务配置表","ad_task_detail": "广告任务轮替表","ad_label": "广告标签配置","ad_strategy": "广告点位策略","battle_pass": "通行证配置项","battle_pass_reward": "通行证奖励配置","rush_limit": "萌萌竞速配置项表","new_user_task_v2": "新手任务配置表","anniversary_common": "周年庆通用配置","anniversary_prize": "周年庆奖品配置","anniversary_prize_type": "周年庆奖品类型配置","anniversary_piece": "周年庆碎片配置","anniversary_product": "周年庆礼包配置","secret_shop_common": "神秘商店配置","secret_shop_condition": "神秘商店条件配置","secret_shop_pool": "神秘商店礼包池配置","secret_shop_gift_bag": "神秘商店礼包配置","mysterious_island_common": "神秘岛通用配置","mysterious_island_reward": "神秘岛奖励配置","purple_amy_magic": "紫咪的魔法配置","ship": "造船赛船只表","ship_chest_grid": "造船赛宝箱格子表","ship_robot_grow": "造船赛机器人成长表","ship_option": "造船赛配置项表","balloon_rise_node": "摘椰子节点配置","balloon_rise_option": "摘椰子通用配置","qualifying_common": "晋级赛通用配置","qualifying_grade": "晋级赛段位配置","qualifying_robot": "晋级赛机器人配置","level_record_fake": "晋级赛机器人保底配置","cube_ab_test": "cubeABtest配置","star_dream": "星岛梦工厂通用配置","star_dream_weight": "星岛梦工厂权重配置","star_dream_reward": "星岛梦工厂奖励配置","level_collection": "主线收集活动配置","endless_treasure_common": "无尽宝藏通用配置","endless_treasure_reward": "无尽宝藏礼包配置","speedboat_match_option": "快艇大赛通用配置","speedboat_match_grade": "快艇大赛赛事级别","month_card": "月卡配置","shop": "新版商城配置","island_treasure": "海岛礼包","sticker_battle_common": "贴贴大作战公共配置","sticker_battle_platform": "贴贴大作战平台配置","h5_ai_daily_bind_package": "每日特惠绑定礼包","gold_elysium_common": "金币乐园公共配置","gold_elysium_robot": "金币乐园机器人配置","fishing_common": "钓鱼达人通用配置","fishing_grade": "钓鱼达人赛事级别配置","fishing_rod": "钓鱼达人钓竿配置","fishing_variety": "钓鱼达人鱼类配置","live_gift_bag": "限定礼包配置","dress": "装扮配置"},
+                {"gacha": "摇一摇活动","time_limit_gift": "限时礼包活动","h5_icon_settings": "活动图标显示配置","h5_shortcut_collect": "快捷收藏","product_wechatgame": "商品配置","product_qq_lightgame": "商品配置-QQ小游戏","product_oppo_quickgame": "商品配置-OPPO小游戏","product_baidu_lightgame": "商品配置-百度小游戏","product_allplatform": "全平台商品配置","goods": "商品清单配置","maxLevel": "最大关卡","compen":"公告配置","jigsaw":"拼图活动","coin_match":"金币大作战活动","goldMatch":"夺宝大冒险活动","game":"游戏通用配置","island":"岛屿配置","level_reward":"关卡奖励","wx_group_share":"加步数分享配置","grayscaleTest":"灰度测试","levelScoreCheck":"分数校验","item":"item配置","prop":"道具配置","baidu_act":"百度清凉活动","friend_pvp":"友谊赛","subscribe":"订阅","friendInteractive":"互动分享","starRewardDraw":"星星抽奖","starRewardDrawItem":"星星抽奖奖品","new_rank":"新排行榜","announcement": "自动公告","adProps": "激励广告","h5_ai_package": "h5_ai礼包","aiGiftBag": "AI礼包参数","groupTest": "正交分组","user_back": "玩家召回","drop_adjust": "掉落优化配置","money_box_product": "存钱罐礼包配置","money_box": "存钱罐通用配置","coast_star": "海滨之星","advertising": "新增视频广告","champion_common": "冠军赛通用配置","champion_ai": "冠军赛ai配置","robot": "机器人配置","level_extend": "关卡配置扩展","ad_task": "广告任务配置表","ad_task_detail": "广告任务轮替表","ad_label": "广告标签配置","ad_source_label": "广告来源标签配置","ad_strategy": "广告点位策略","ad_source_strategy": "广告来源点位策略配置","battle_pass": "通行证配置项","battle_pass_reward": "通行证奖励配置","rush_limit": "萌萌竞速配置项表","new_user_task_v2": "新手任务配置表","anniversary_common": "周年庆通用配置","anniversary_prize": "周年庆奖品配置","anniversary_prize_type": "周年庆奖品类型配置","anniversary_piece": "周年庆碎片配置","anniversary_product": "周年庆礼包配置","anniversary_battle_pass": "周年庆通行证配置","secret_shop_common": "神秘商店配置","secret_shop_condition": "神秘商店条件配置","secret_shop_pool": "神秘商店礼包池配置","secret_shop_gift_bag": "神秘商店礼包配置","mysterious_island_common": "神秘岛通用配置","mysterious_island_reward": "神秘岛奖励配置","purple_amy_magic": "紫咪的魔法配置","ship": "造船赛船只表","ship_chest_grid": "造船赛宝箱格子表","ship_robot_grow": "造船赛机器人成长表","ship_option": "造船赛配置项表","balloon_rise_node": "摘椰子节点配置","balloon_rise_option": "摘椰子通用配置","balloon_rise_drop_optimize": "摘椰子掉落优化干预","ai_prop_price": "ai-道具标价","qualifying_common": "晋级赛通用配置","qualifying_grade": "晋级赛段位配置","qualifying_robot": "晋级赛机器人配置","level_record_fake": "晋级赛机器人保底配置","cube_ab_test": "cubeABtest配置","star_dream": "星岛梦工厂通用配置","star_dream_weight": "星岛梦工厂权重配置","star_dream_reward": "星岛梦工厂奖励配置","level_collection": "主线收集活动配置","endless_treasure_common": "无尽宝藏通用配置","endless_treasure_reward": "无尽宝藏礼包配置","speedboat_match_option": "快艇大赛通用配置","speedboat_match_grade": "快艇大赛赛事级别","month_card": "月卡配置","shop": "新版商城配置","island_treasure": "海岛礼包","sticker_battle_common": "贴贴大作战公共配置","sticker_battle_platform": "贴贴大作战平台配置","h5_ai_daily_bind_package": "每日特惠绑定礼包","gold_elysium_common": "金币乐园公共配置","gold_elysium_robot": "金币乐园机器人配置","gold_elysium_robot_v2": "金币乐园机器人配置V2","fishing_common": "钓鱼达人通用配置","fishing_grade": "钓鱼达人赛事级别配置","fishing_rod": "钓鱼达人钓竿配置","fishing_variety": "钓鱼达人鱼类配置","magic_code_option": "解码特工配置项","magic_code_dan": "解码特工段位表","magic_code_drop_optimize": "解码特工掉落优化干预表","dunk_ace_dan": "投篮王牌积分段位表","dunk_ace_drop_optimize": "投篮王牌掉落干预表","dunk_ace_robot_grow": "投篮王牌机器人成长干涉表","dunk_ace_option": "投篮王牌配置项表","water_fighting_dan": "水上乐斗累计胜利段位表","water_fighting_round_reward": "水上乐斗场次奖励表","water_fighting_option": "水上乐斗配置项表","trinity_gift_bag_option": "三选一礼包配置项","trinity_gift_bag": "三选一礼包","tetris_option": "俄罗斯方块配置项表","salvage_gift_bag_option": "新手关末救援礼包配置项表","salvage_gift_bag": "新手关末救援礼包表","alchemy_option": "炼金工坊配置项表","alchemy_task": "炼金工坊任务表","mail_template": "信件模板表","mail_system": "信件全服邮件表","live_gift_bag": "限定礼包配置","dress": "装扮配置","level_score_rank": "关卡分数排行配置","circus_game":"马戏团配置","ocean_party_common":"海洋派对公共配置","ocean_party_platform":"海洋派对平台配置","coffee_lot_cooking_common":"栗熊厨房通用配置","coffee_lot_cooking_grade":"栗熊厨房阶段配置","coffee_lot_cooking_food":"栗熊厨房食材配置","super_magic_bird_common":"超鸟配置项表","super_magic_bird_strategy":"超鸟策略配置总表","props_consume_value":"关内道具消耗价值表","pet_story_common":"萌宠物语通用配置","pet_story_robot":"萌宠物语机器人配置","pet_story_category":"萌宠物语分类配置","activity_rewards_convert":"联动活动奖励折算配置","add_step_video_expand":"关末广告加步数膨胀配置","conditional_pack":"条件礼包配置"},
                 // config_client.json
-                {"strings": "文本配置","promot": "互导推广配置","share_group": "分享组配置","share_default": "分享默认配置","gacha": "摇一摇活动","time_limit_gift": "限时礼包活动","product_wechatgame": "前端-商品配置","product_qq_lightgame": "商品配置-QQ小游戏","product_oppo_quickgame": "商品配置-OPPO小游戏","product_baidu_lightgame": "商品配置-百度小游戏","goods": "商品清单配置","icon_settings": "活动图标显示配置","island": "岛屿配置","intro": "岛屿新内容配置","introB": "岛屿新内容配置B","coin_match": "金币大作战活动","game": "游戏通用配置","level_extend": "关卡配置扩展","shortcut": "快捷收藏","product_allplatform": "全平台商品配置","prop": "道具配置","level_baiduxiaoshu": "百度清凉活动","wx_group_share": "加步数分享配置","friend_pvp": "友谊赛","subscribe_msg":"订阅","interactive_invite": "互动分享","new_chest": "星星抽奖","newrank_province": "新排行榜地区配置","newrank": "新排行榜","ad_props": "激励广告","aipackage_settings": "AI礼包参数","aipackage": "h5_ai礼包","orthogonal_group": "正交分组","return_back": "玩家召回","dropadjust_apply": "掉落优化应用","dropadjust": "掉落优化配置","dropadjust_difftag": "掉落优化难度标签条件","dropadjust_orthgroup": "正交分组配置","dropadjust_orthapply": "正交策略配置","piggy_bank_config": "存钱罐礼包配置","piggy_bank_normal_config": "存钱罐通用配置","seaside_star_config": "海滨之星","privacy_config": "隐私配置","champion_config": "冠军赛通用配置","new_ad_config": "新增视频广告","adtask_common_config": "广告任务配置表","adtask_task_config": "广告任务轮替表","ad_label": "广告标签配置","ad_strategy": "广告点位策略","battlepass_config": "通行证配置项","lightning_rush": "萌萌竞速配置项表","drop_intervention": "新掉落干预总表","dropadjust_difftag_new": "新掉落优化难度标签条件","cube_ab_test": "cubeABtest配置","shardcollect_config": "周年庆通用配置","shardcollect_goods_config": "周年庆奖品配置","shardcollect_gift_config": "周年庆礼包配置","shardcollect_piece_config" : "周年庆碎片配置","special_shop_config": "神秘商店配置","special_shop_gift_config": "神秘商店礼包配置","buildship_config": "造船赛配置项表","buildship_box_config": "造船赛宝箱格子表","mysterious_island_common": "神秘岛通用配置","qualify_common": "晋级赛通用配置","qualify_grade": "晋级赛段位配置","stardream_config" : "星岛梦工厂通用配置","stardream_reward_config" : "星岛梦工厂奖励配置","endless_treasure_config" : "无尽宝藏通用配置","endless_treasure_gift_config" : "无尽宝藏礼包配置","speedboat_match_option": "快艇大赛通用配置","speedboat_match_grade": "快艇大赛赛事级别","month_ticket" : "月卡配置","welfare_center_config" : "福利中心配置","islandtreasure_config" : "海岛礼包","fail_times_optimize_config" : "关末长尾配置","shop": "新版商城配置","ai_daily_bind_package" : "每日特惠绑定礼包","sticker_battle_common" : "贴贴大作战公共配置","sticker_battle_platform" : "贴贴大作战平台配置","pick_coco_common": "摘椰子通用配置","pick_coco_config": "摘椰子节点配置","dynamic_guide" : "动态引导配置","gold_elysium_common": "金币乐园公共配置","fishing_variety" : "钓鱼达人鱼类配置","fishing_grade" : "钓鱼达人赛事级别配置","fishing_common" : "钓鱼达人通用配置","fishing_rod" : "钓鱼达人钓竿配置","dress" : "装扮配置"}
+                {"strings": "文本配置","promot": "互导推广配置","share_group": "分享组配置","share_default": "分享默认配置","gacha": "摇一摇活动","time_limit_gift": "限时礼包活动","product_wechatgame": "前端-商品配置","product_qq_lightgame": "商品配置-QQ小游戏","product_oppo_quickgame": "商品配置-OPPO小游戏","product_baidu_lightgame": "商品配置-百度小游戏","goods": "商品清单配置","icon_settings": "活动图标显示配置","island": "岛屿配置","intro": "岛屿新内容配置","introB": "岛屿新内容配置B","coin_match": "金币大作战活动","game": "游戏通用配置","level_extend": "关卡配置扩展","shortcut": "快捷收藏","product_allplatform": "全平台商品配置","prop": "道具配置","level_baiduxiaoshu": "百度清凉活动","wx_group_share": "加步数分享配置","friend_pvp": "友谊赛","subscribe_msg":"订阅","interactive_invite": "互动分享","new_chest": "星星抽奖","newrank_province": "新排行榜地区配置","newrank": "新排行榜","ad_props": "激励广告","aipackage_settings": "AI礼包参数","aipackage": "h5_ai礼包","orthogonal_group": "正交分组","return_back": "玩家召回","dropadjust_apply": "掉落优化应用","dropadjust": "掉落优化配置","dropadjust_difftag": "掉落优化难度标签条件","dropadjust_orthgroup": "正交分组配置","dropadjust_orthapply": "正交策略配置","piggy_bank_config": "存钱罐礼包配置","piggy_bank_normal_config": "存钱罐通用配置","seaside_star_config": "海滨之星","privacy_config": "隐私配置","champion_config": "冠军赛通用配置","new_ad_config": "新增视频广告","adtask_common_config": "广告任务配置表","adtask_task_config": "广告任务轮替表","ad_label": "广告标签配置","ad_strategy": "广告点位策略","ad_source_strategy": "广告来源点位策略配置","battlepass_config": "通行证配置项","lightning_rush": "萌萌竞速配置项表","drop_intervention": "新掉落干预总表","dropadjust_difftag_new": "新掉落优化难度标签条件","cube_ab_test": "cubeABtest配置","shardcollect_config": "周年庆通用配置","shardcollect_goods_config": "周年庆奖品配置","shardcollect_gift_config": "周年庆礼包配置","shardcollect_pass_config": "周年庆通行证配置","shardcollect_piece_config" : "周年庆碎片配置","special_shop_config": "神秘商店配置","special_shop_gift_config": "神秘商店礼包配置","buildship_config": "造船赛配置项表","buildship_box_config": "造船赛宝箱格子表","mysterious_island_common": "神秘岛通用配置","qualify_common": "晋级赛通用配置","qualify_grade": "晋级赛段位配置","stardream_config" : "星岛梦工厂通用配置","stardream_reward_config" : "星岛梦工厂奖励配置","endless_treasure_config" : "无尽宝藏通用配置","endless_treasure_gift_config" : "无尽宝藏礼包配置","speedboat_match_option": "快艇大赛通用配置","speedboat_match_grade": "快艇大赛赛事级别","month_ticket" : "月卡配置","welfare_center_config" : "福利中心配置","islandtreasure_config" : "海岛礼包","fail_times_optimize_config" : "关末长尾配置","shop": "新版商城配置","ai_daily_bind_package" : "每日特惠绑定礼包","sticker_battle_common" : "贴贴大作战公共配置","sticker_battle_platform" : "贴贴大作战平台配置","pick_coco_common": "摘椰子通用配置","pick_coco_config": "摘椰子节点配置","dynamic_guide" : "动态引导配置","gold_elysium_common": "金币乐园公共配置","fishing_variety" : "钓鱼达人鱼类配置","fishing_grade" : "钓鱼达人赛事级别配置","fishing_common" : "钓鱼达人通用配置","fishing_rod" : "钓鱼达人钓竿配置","dress" : "装扮配置","level_score_rank" : "关卡分数排行配置","magic_code_option": "解码特工配置项","magic_code_dan": "解码特工段位表","magic_code_drop_optimize": "解码特工掉落优化干预表","circus_config" : "马戏团配置","ocean_party_common":"海洋派对公共配置","coffee_lot_cooking_common": "栗熊厨房通用配置","coffee_lot_cooking_grade": "栗熊厨房阶段配置","coffee_lot_cooking_food": "栗熊厨房食材配置","popout_activity_a": "强弹配置A","popout_scene_a": "强弹场景配置A","popout_activity_b": "强弹配置B","popout_scene_b": "强弹场景配置B","dunkace_config": "投篮王牌配置项表","dunkace_rewards_config": "投篮王牌积分段位表","prop_consume_value": "关内道具消耗价值表","water_fighting_option": "水上乐斗配置项表","water_fighting_round_reward": "水上乐斗场次奖励表","water_fighting_dan": "水上乐斗累计胜利段位表","nuclear_bird_option": "超鸟配置项表","trinity_gift_bag_option": "三选一礼包配置项","trinity_gift_bag": "三选一礼包","tetris_config": "俄罗斯方块配置项表","salvage_gift_bag_option": "新手关末救援礼包配置项表","salvage_gift_bag": "新手关末救援礼包表","activity_rewards_convert":"联动活动奖励折算配置","pet_story_common": "萌宠物语通用配置","alchemy_option":"炼金工坊配置项表","alchemy_task": "炼金工坊任务表"}
             ]
         };
         extraShowConf["shtcn0tNNgGf2ufrGRO0qIUhbBg"] = extraShowConf["shtcnnjxBfNgfXAYAOoN3ETcM8f"]; // H5：数据总表【dev】
@@ -192,10 +274,18 @@
     }
 
 
-    function genFinderHtmlCode() {
+    function genSheetFinderHtmlCode() {
         var htmlCode = '' +
-            '<div id="ld-feishu-doc-finder" >' + // style="position:fixed; z-index:9999; left:500px; top:10px; display:none;"
-            '  <div style="padding:8px 10px 5px 10px;"><input id="ld-feishu-doc-finder-text" type="text" style="width: 100%; height:30px; padding:0 5px;" placeholder="输入页签名进行搜索" autocomplete="off"></div>' +
+            '<div id="ld-feishu-sheet-finder" >' + // style="position:fixed; z-index:9999; left:500px; top:10px; display:none;"
+            '  <div style="padding:8px 10px 5px 10px;"><input id="ld-feishu-sheet-finder-text" type="text" style="width: 100%; height:30px; padding:0 5px;" placeholder="输入页签名进行搜索" autocomplete="off"></div>' +
+            '</div>';
+        return htmlCode;
+    }
+
+    function genExcelFinderHtmlCode() {
+        var htmlCode = '' +
+            '<div id="ld-feishu-excel-finder" >' + // style="position:fixed; z-index:9999; left:500px; top:10px; display:none;"
+            '  <div style="padding:3px 10px 3px 3px;"><input id="ld-feishu-excel-finder-text" type="text" style="width: 100%; height:30px; padding:0 5px;" placeholder="输入文件名进行搜索" autocomplete="off"></div>' +
             '</div>';
         return htmlCode;
     }
